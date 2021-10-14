a7aa22f158b30260f7ca191a0ede68e924805561
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-14 11:45:59 +0000

    Deploy hailiting/FantomPanda to github.com/hailiting/FantomPanda.git:gh-pages

---
+++ precache-manifest.094b2c823857475dd2a5f3dcbb0fc642.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273bc7094a949602d5ba7efbe4c4074e",
+    "url": "/FantomPanda/index.html"
+  },
+  {
+    "revision": "89a237440fadd53d2e76",
+    "url": "/FantomPanda/static/css/3.de616d38.chunk.css"
+  },
+  {
+    "revision": "3990bc36de1299f38702",
+    "url": "/FantomPanda/static/css/main.9a947f9b.chunk.css"
+  },
+  {
+    "revision": "3f7bc90d431216bb8991",
+    "url": "/FantomPanda/static/js/2.c936da2f.chunk.js"
+  },
+  {
+    "revision": "d02ec6d2778552254da8249efee038aa",
+    "url": "/FantomPanda/static/js/2.c936da2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a237440fadd53d2e76",
+    "url": "/FantomPanda/static/js/3.fa67c130.chunk.js"
+  },
+  {
+    "revision": "0796e397eba6afa42186",
+    "url": "/FantomPanda/static/js/4.04097ce8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/FantomPanda/static/js/4.04097ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3990bc36de1299f38702",
+    "url": "/FantomPanda/static/js/main.13e07988.chunk.js"
+  },
+  {
+    "revision": "52c3d95af94c1c912a02",
+    "url": "/FantomPanda/static/js/runtime-main.35cc7b5c.js"
+  },
+  {
+    "revision": "a1302047bc69e7ca9ffef060d6326e61",
+    "url": "/FantomPanda/static/media/01.a1302047.png"
+  },
+  {
+    "revision": "c73c3b887828d7f278126af206f25b1d",
+    "url": "/FantomPanda/static/media/02.c73c3b88.png"
+  },
+  {
+    "revision": "3395e2adca0e3f6e23b1525d2556e435",
+    "url": "/FantomPanda/static/media/03.3395e2ad.png"
+  },
+  {
+    "revision": "0f8edcf36d63bfccf164b25a14bcff07",
+    "url": "/FantomPanda/static/media/04.0f8edcf3.png"
+  },
+  {
+    "revision": "10796b92c7eea09eb6600987d59691a6",
+    "url": "/FantomPanda/static/media/05.10796b92.png"
+  },
+  {
+    "revision": "13924dacd2f7f201e3336afc2c57bada",
+    "url": "/FantomPanda/static/media/06.13924dac.png"
+  },
+  {
+    "revision": "6f9f7f27cb5f0b0b477d2ae26fb5c387",
+    "url": "/FantomPanda/static/media/07.6f9f7f27.png"
+  },
+  {
+    "revision": "24044740dff6b5ff78c6c52a229e5dd2",
+    "url": "/FantomPanda/static/media/08.24044740.png"
+  },
+  {
+    "revision": "d9792e3f81e3496e750a65d0632fca69",
+    "url": "/FantomPanda/static/media/logo.d9792e3f.svg"
+  },
+  {
+    "revision": "e23ce63be8e8b48f841c2b0df8d85849",
+    "url": "/FantomPanda/static/media/partfourbg.e23ce63b.png"
+  },
+  {
+    "revision": "8f612f215c33fb6ae7009fbdddea9320",
+    "url": "/FantomPanda/static/media/partonebg.8f612f21.png"
+  },
+  {
+    "revision": "38d4452676371e5d9c4a8524959a4c47",
+    "url": "/FantomPanda/static/media/parttwobg.38d44526.png"
+  },
+  {
+    "revision": "83897a3e8ad628249bef6fe947e9e4d0",
+    "url": "/FantomPanda/static/media/races_01.83897a3e.png"
+  },
+  {
+    "revision": "744441ea6483fd3673563f1fc5f45c72",
+    "url": "/FantomPanda/static/media/races_0103.744441ea.png"
+  },
+  {
+    "revision": "ee4523a61490635b0931e48620da9d5b",
+    "url": "/FantomPanda/static/media/races_02.ee4523a6.png"
+  },
+  {
+    "revision": "7b629ebd71c42ddb469affdf979cbf19",
+    "url": "/FantomPanda/static/media/races_0202.7b629ebd.png"
+  },
+  {
+    "revision": "353613af09d0d7cb45e1f644de523e21",
+    "url": "/FantomPanda/static/media/races_0203.353613af.png"
+  },
+  {
+    "revision": "3cb1926a34a89bdff6332e8955fa785f",
+    "url": "/FantomPanda/static/media/races_03.3cb1926a.png"
+  },
+  {
+    "revision": "49a4bd875ec1e548637fb9460cf05284",
+    "url": "/FantomPanda/static/media/races_0301.49a4bd87.png"
+  },
+  {
+    "revision": "7e8b8e4aa85490d15436ff82d363cc2c",
+    "url": "/FantomPanda/static/media/races_0303.7e8b8e4a.png"
+  },
+  {
+    "revision": "9b5c6ff7f3d0b81e66010d3030de9371",
+    "url": "/FantomPanda/static/media/races_04.9b5c6ff7.png"
+  },
+  {
+    "revision": "8d915c565c09c741f6de20df2854b8d2",
+    "url": "/FantomPanda/static/media/races_0401.8d915c56.png"
+  },
+  {
+    "revision": "aab31a68ba76d168d016af218bd2d122",
+    "url": "/FantomPanda/static/media/races_0402.aab31a68.png"
+  },
+  {
+    "revision": "8eb7e843df4330c749b3d87b3aedc86f",
+    "url": "/FantomPanda/static/media/races_0403.8eb7e843.png"
+  },
+  {
+    "revision": "b10893f767b5f0926c6b906bdd95659c",
+    "url": "/FantomPanda/static/media/six01.b10893f7.png"
+  },
+  {
+    "revision": "b2e6cdc4cae8f97ed57adab4a2670902",
+    "url": "/FantomPanda/static/media/six02.b2e6cdc4.png"
+  },
+  {
+    "revision": "02ee9e30d0f3ae94f1ca80ed704efc27",
+    "url": "/FantomPanda/static/media/six03.02ee9e30.png"
+  }
+]);
